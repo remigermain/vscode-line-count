09f7330af927a4dba0fca94d85dd24ae5511f71b
rgermain <contact@germainremi.fr>
2024-11-10 00:31:16 +0100

    fix: selected first lines

--- src/extension.ts
+++ src/extension.ts
@@ -40,9 +40,16 @@ function onChangeStatusBar(statusBar: vscode.StatusBarItem): void {
 
   editor.selections.forEach((el) => {
     // selected lines
-    let ln = el.end.line - el.start.line || 1
-    if (el.end.character === 0) {
+    let ln = el.end.line - el.start.line
+
+    if (ln === 0) {
+      if (el.start.character !== el.end.character) {
+        ln += 1
+      }
+    } else if (el.end.character === 0) {
       ln -= 1;
+    } else {
+      ln += 1
     }
     info.selected.ln += ln
 
@@ -54,7 +61,7 @@ function onChangeStatusBar(statusBar: vscode.StatusBarItem): void {
 
   statusBar.text = `Ln ${info.ln}, Col ${info.col}`;
   if (info.selected.ln > 0 || info.selected.word > 0) {
-    statusBar.text += `(Ln ${info.selected.ln + 1}, ${info.selected.word} Selected)`
+    statusBar.text += `(Ln ${info.selected.ln}, ${info.selected.word} Selected)`
   }
   statusBar.show();
 }
